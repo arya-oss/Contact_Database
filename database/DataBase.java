560683995750dcdd436e9c67656c45f5760de3c6
Rajmani Arya <rajmani1995@gmail.com>
2015-05-25 14:24:36 +0530

    Update DataBase.java

--- database/DataBase.java
+++ database/DataBase.java
@@ -1,32 +1,37 @@
-package database;
-
-import java.sql.*;
-import java.util.logging.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @author Rajmani Arya
+ * @see www.rajmani1995.blogspot.com
  */
-public class DataBase {
-    private static DataBase instance=null;
-    private Connection conn;
-    private DataBase() {
-        
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shubhayatra", "root", "");
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(DataBase.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    public static Connection getConnection() {
-        if(instance == null) {
-            instance = new DataBase();
-        }
-        return instance.conn;
-    }
-    public static void main(String[] args) throws SQLException {
-        Connection conn = DataBase.getConnection();
-        conn.close();
-        System.out.println("Connected !");
-    }
+public class DatabaseConnection implements DbConfig {
+	private static DatabaseConnection instance=null;
+	private Connection conn;
+	public static Connection getInstance() {
+		if(instance==null)
+			instance = new DatabaseConnection();
+		return instance.conn;
+	}
+	private DatabaseConnection() {
+		String dburl = "jdbc:mysql://"+Host+":"+Port+"/"+Database;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dburl, User, Password);
+		} catch (ClassNotFoundException e ) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		Connection conn = DatabaseConnection.getInstance();
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("connected !");
+	}
 }
