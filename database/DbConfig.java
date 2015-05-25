992c6e152fb1bfa291648dcc8ef39a980e4c413f
Rajmani Arya <rajmani1995@gmail.com>
2015-05-25 14:27:47 +0530

    Create DbConfig.java

---
+++ database/DbConfig.java
@@ -0,0 +1,7 @@
+public interface DbConfig {
+    final String Host = "127.0.0.1";
+    final int Port = 3306;
+    final String User = "root";
+    final String Password = "";
+    final String Database = "test";
+}
